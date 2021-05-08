a35d48f4464be51686a2e21953ed2dcd1f792218
oussama <mohamedoussamabenjazia@gmail.com>
2021-05-08 15:30:51 +0200

    first commit

---
+++ quizscripts/first try/AnswerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class AnswerScript : MonoBehaviour
+{
+
+public bool isCorrect = false; 
+public QuizManager quizzManager ; 
+
+
+
+
+public void Answer(){
+
+
+    if (isCorrect)
+    {
+
+        Debug.Log("Correct Answer");
+        quizzManager.correct();
+    }
+    else
+    {
+       Debug.Log("wrong Answer"); 
+       quizzManager.correct() ;
+    }
+
+
+
+
+}
+}
